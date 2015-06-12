036d6e8d20f116b02e32118b15cbd0f1402b9d0d
Daniel Bergmann <brgmnn@users.noreply.github.com>
2015-06-12 16:30:49 +0100

    Fish: added descriptions to the svn functions.

--- fish/.config/fish/functions/svn.fish
+++ fish/.config/fish/functions/svn.fish
@@ -1,15 +1,15 @@
-function _svn_revision
+function _svn_revision --description "Returns the current SVN revision number."
     echo -ns (set_color $fish_color_quote) \
             (svn info | grep "Revision" | sed -e 's/Revision: //') \
             (set_color normal)
 end
 
-function _svn_branch_name
+function _svn_branch_name --description "Returns the current SVN branch name."
     echo -ns (set_color blue)(svn info | sed -n "/URL:/s/.*\///p") \
             (set_color normal)
 end
 
-function _svn_dirty
+function _svn_dirty --description "Returns how many files are changed/added/removed."
     set -l edits (svn status -q | grep '^M\|^D\|^A' | wc -l)
     if test $edits -gt 0
         echo -n (set_color $fish_color_status)"$edits*"(set_color normal)
