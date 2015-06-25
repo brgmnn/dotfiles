119e739c86454d1ad916771b1fb00301aff4c99a
Daniel Bergmann <brgmnn@users.noreply.github.com>
2015-06-25 17:05:24 +0100

    Firefox: Added firefox user preferences JS file.

---
+++ firefox/.mozilla/firefox/daniel.profile/user.js
@@ -0,0 +1,8 @@
+user_pref("browser.newtab.url", "about:newtab");
+
+// What to display on startup, 3 is previous tabs and windows.
+user_pref("browser.startup.page", 3);
+user_pref("browser.tabs.warnOnClose", false);
+
+user_pref("network.http.pipelining", true);
+user_pref("network.http.proxy.pipelining", true);
