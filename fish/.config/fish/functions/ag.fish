8552a60d498581a5012d293397cd0d527a760f30
Daniel Bergmann <brgmnn@users.noreply.github.com>
2015-11-05 16:13:42 -0800

    Fish: added custom colours to Ag.

---
+++ fish/.config/fish/functions/ag.fish
@@ -0,0 +1,8 @@
+function ag --description "Ag, the silver surfer."
+    command ag --color-path '38;5;121' --color-line-number '38;5;39' \
+            --color-match '30m[48;5;222' $argv
+end
+
+function Ag --description "Ag, the silver surfer."
+    ag $argv
+end
