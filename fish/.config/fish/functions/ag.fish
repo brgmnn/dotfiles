f050df0dd834fd05860d134f647c51d84c472dc6
Daniel Bergmann <brgmnn@users.noreply.github.com>
2015-11-17 10:45:20 -0800

    Fish: Changed Ag match highlighting colour.

--- fish/.config/fish/functions/ag.fish
+++ fish/.config/fish/functions/ag.fish
@@ -1,6 +1,6 @@
 function ag --description "Ag, the silver surfer."
     command ag --color-path '38;5;121' --color-line-number '38;5;39' \
-            --color-match '30m[48;5;222' $argv
+            --color-match '30m[48;5;154' $argv
 end
 
 function Ag --description "Ag, the silver surfer."
