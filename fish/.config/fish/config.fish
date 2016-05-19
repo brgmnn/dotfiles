53f41de0c717297b70704bb639d401a5736e61da
Daniel Bergmann <brgmnn@users.noreply.github.com>
2016-05-19 09:40:50 -0700

    Fish: Removed abbreviations for git pull/push.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -25,8 +25,6 @@ abbr -a gs='git status'
 abbr -a gd='git diff'
 abbr -a gc='git commit'
 abbr -a ga='git add'
-abbr -a gd='git pull'
-abbr -a gu='git push'
 abbr -a gca='git commit --amend'
 abbr -a gbr='git branch -vv'
 
