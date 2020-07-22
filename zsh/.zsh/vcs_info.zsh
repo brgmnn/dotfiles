64945a6d0cb62a3086bf8107d9dc89fede2676e1
Daniel Bergmann <brgmnn@users.noreply.github.com>
2020-07-21 18:15:10 -0700

    Zsh: Remove path from git prompt

--- zsh/.zsh/vcs_info.zsh
+++ zsh/.zsh/vcs_info.zsh
@@ -10,8 +10,7 @@ zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr '*'
 zstyle ':vcs_info:*' stagedstr   '+'
 zstyle ':vcs_info:*' formats \
-    "%{$fg[grey]%}%s%{$reset_color%} %r/%S%{$fg[grey]%} \
-%{$fg[blue]%}%b%{$reset_color%}%B%F{1}%u%f%F{2}%c%{$reset_color%} "
+    "%{$fg[blue]%}%b%{$reset_color%}%B%F{1}%u%f%F{2}%c%{$reset_color%} "
 
 zstyle ':vcs_info:*' actionformats \
     '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
