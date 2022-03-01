ba502c08901c9a1afd0efcc7ef21c5aa78e95d20
Daniel Bergmann <brgmnn@users.noreply.github.com>
2022-03-01 12:20:14 -0800

    Zsh: add nvm chpwd script

--- zsh/.zsh/nvm.zsh
+++ zsh/.zsh/nvm.zsh
@@ -7,23 +7,24 @@ export NVM_DIR="$HOME/.nvm"
 # place this after nvm initialization!
 autoload -U add-zsh-hook
 
+# Automatically load the correct node version when changing directory. This is a stripped down
+# version of one more commonly seen in other files that only checks the current directory rather
+# than any nvmrc file in the file path. This is to keep cd's fast when switching subdirectories
+# within a project folder.
 load-nvmrc() {
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
+    # local nvmrc_path="$(nvm_find_nvmrc)"
 
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+    if [[ -e .nvmrc ]]; then
+        local node_version="$(nvm version)"
+        local nvmrc_node_version=$(nvm version "$(cat .nvmrc)")
 
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
+        if [[ "$nvmrc_node_version" = "N/A" ]]; then
+            echo "Node $nvmrc_node_version is missing. Install with: nvm install"
+        elif [[ "$nvmrc_node_version" != "$node_version" ]]; then
+            nvm use
+        fi
     fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
 }
 
-# add-zsh-hook chpwd load-nvmrc
-# load-nvmrc
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
