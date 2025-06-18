89723f4b2d53705d0e2fc2bfba727a0e768d66af
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-06-17 21:17:19 -0700

    Mac: Add more process restarts

--- macos/configure.sh
+++ macos/configure.sh
@@ -4,4 +4,11 @@
 defaults write com.apple.dock tilesize -int 48
 defaults write com.apple.dock showhidden -bool false
 
+# Keyboard
+
+
+# Restart items
 killall Dock
+killall Finder
+killall SystemUIServer
+killall NotificationCenter
